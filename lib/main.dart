100393ca38d7657e0a3287b0cefda86f08a064cc
Andi-IM <andi.irhamm@gmail.com>
2025-09-16 09:21:00 +0700

    feat(firebase): add firebase configuration and initialization
    - Add firebase.json configuration file
    - Configure Firebase services in Android build files
    - Initialize Firebase in main application entry point
    - Add generated Firebase options file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lokapandu/app.dart';
 import 'package:lokapandu/env/env.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: Env.supabaseUrl,
-    anonKey: Env.supabaseKey,
-  );
-  
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseKey);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const App());
 }
